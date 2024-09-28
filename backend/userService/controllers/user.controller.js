9b59f8c80ba436b40528ba234045c727eff8e906
DakshiArora <medakshi11@gmail.com>
2024-09-28 16:59:00 +0530

    mcv

--- backend/userService/controllers/user.controller.js
+++ backend/userService/controllers/user.controller.js
@@ -36,4 +36,51 @@ const getUser = async (req, res) => {
     }
 };
 
-module.exports = { registerUser, getUser };
+const getUserById = async(req,res) => {
+    try{
+        User.findById(req.params.id)
+        .then(data => res.send(data))
+        .catch(err => res.send('something went wrong'))
+    }catch(err){
+        console.log(err)
+        res.send('ERR')
+    }
+}
+
+const updateUserById = async(req,res) => {
+    try{
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+            },
+            {
+                new: true
+            }
+        )
+        if(!updatedUser){
+           return res.status(404).send('User Not found')
+        }
+        res.send(updatedUser)
+    }catch(err){
+        console.log(err)
+        res.send('ERR')
+    }
+}
+
+const deleteUserById = async(req,res) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.params.id)
+        if(!deletedUser){
+            return res.status(404).send('User Not found')
+        }
+        res.send('User deleted successfully')
+    }catch(err){
+
+    }
+}
+
+module.exports = { registerUser,getUser, getUserById, updateUserById, deleteUserById }
+
