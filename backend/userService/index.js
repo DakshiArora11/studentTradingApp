b47cb5e2e27f95d9a94c35662936a11908347ab3
DakshiArora <medakshi11@gmail.com>
2024-09-28 16:19:35 +0530

    mcv

--- backend/userService/index.js
+++ backend/userService/index.js
@@ -1,5 +1,26 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+const helloRoutes = require('./routes/hello.route')
+const userRoutes = require('./routes/user.route')
+app.use('/api/v1/', helloRoutes)
+app.use('/api/v1/user', userRoutes)
+
+// username, email, password, 
+// user registration --> username, email, password (user)
+// user details --> bio, name, age, gender, phoneNum, enrollmentNum, 
+//department, batch, semester, school (school name, percentage marks, city), 
+//city, country, hobby, achievements, profilePic  (userdetails)
+// 
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server started at port ${process.env.PORT}`)
+})
+
+
+
 //npm init
-const express  = require('express')
+/* const express  = require('express')
 const abc= require ('\.xyz')
 const app=express()
 const person=[{
@@ -45,3 +66,4 @@ app.listen(3000, ()=>{
 //npm load //gitHub: push 
 //git add . 
 //git commit -a 
+*/
